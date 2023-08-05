1439803bb15b151483b0a6a675b274b8ae8d1628
sophiecchen <116760795+sophiecchen@users.noreply.github.com>
2023-08-04 20:39:09 -0400

    Update main.py

--- py_test_pack/main.py
+++ py_test_pack/main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import re
+from re import match
 
 def alphabet(text):
     regex = "[a-zA-Z]+"
     regex += "at"
-    match = re.match(regex, text)
+    matcher = match(regex, text)
     
-    if match:
+    if matcher:
         return True
     else:
         return False
@@ -17,9 +17,9 @@ def numbers(text):
     regex = "[1-9]"
     regex += "[0-9]*"
     regex += "%"
-    match = re.match(regex, text)
+    matcher = match(regex, text)
     
-    if match:
+    if matcher:
         return True
     else:
         return False
